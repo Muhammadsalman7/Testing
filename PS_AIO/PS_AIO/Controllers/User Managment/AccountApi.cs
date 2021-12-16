dcf7d94760e2d34479b2381722440c8b6373a1a4
muhammad salman <salman-attari@LIVE.COM>
2021-12-16 23:45:48 +0500

    Publish By Muhammad Salamn

---
+++ PS_AIO/PS_AIO/Controllers/User Managment/AccountApi.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS_AIO.Controllers.User_Managment
+{
+    public class AccountApi : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
